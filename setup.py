6b1508487df12e2866faba0ed517d3493d60dd4f
kmaasrud <kmaasrud@outlook.com>
2020-08-04 10:21:07 +0200

    Added some testing

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='obsidian-hugo',
+      version='0.1',
+      description='Makes Obsidian documents Hugo-friendly',
+      url='https://github.com/kmaasrud/obsidian-hugo',
+      author='kmaasrud',
+      author_email='kmaasrud@outlook.com',
+      license='MIT',
+      packages=['obsidian-hugo'],
+      zip_safe=False)
