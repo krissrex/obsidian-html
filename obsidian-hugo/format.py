23defc3683fa8f1ab76a54bc4d371bc4f82570ed
kmaasrud <kmaasrud@outlook.com>
2020-08-04 13:25:59 +0200

    Temporary testing with my vault

--- obsidian-hugo/format.py
+++ obsidian-hugo/format.py
@@ -1,29 +1,22 @@
 import re
 
 def format_internal_links(document):
-    """Formats Obsidian internal links"""
-    # Links that are neither aliased, nor link to headers
-    out = re.sub("\\[{2}([^|#]*?)\\]{2}", "[\\1](\"\\1.md\")", document)
-    # Aliased links
-    out = re.sub("\\[{2}([^|#\\]]*?)\\|(.*?)\\]{2}", "[\\2](\"\\1.md\")", out)
-    # Header links (TODO: Headers need to be slug case formatted, so this does not work right now)
-    out = re.sub("\\[{2}([^|#\\]]*?)#(.*?)\\]{2}", "[\\2](\"\\1.md#\\2\")", out)
+    """Links that are neither aliased, nor link to headers"""
+    return re.sub("\\[{2}([^|#]*?)\\]{2}", "[\\1]({{ ref \"\\1.md\" }})", document)
 
-    return out
+def format_internal_aliased_links(document):
+    """Aliased links"""
+    return re.sub("\\[{2}([^|#\\]]*?)\\|(.*?)\\]{2}", "[\\2]({{ ref \"\\1.md\" }})", document)
 
-doc = """[[rclone]] is easily installed on [[Arch Linux]] via [[Arch pacman|pacman]]:
+def format_internal_header_links(document):
+    """Header links (TODO: Headers need to be slug case formatted, so this does not work right now)"""
+    return re.sub("\\[{2}([^|#\\]]*?)#(.*?)\\]{2}", "[\\2]({{ ref \"\\1.md#\\2\" }})", document)
 
-	sudo pacman -S rclone
-	
-1. Use `rclone config` and follow the steps as wished
+with open("/home/kmaasrud/Brain/Obsidian and GitHub Pages workflow.md") as f:
+    doc = f.read()
 
-	- Most defaults are fine
-	- There's no need for a `client-id` or a `client-secret`
+doc = format_internal_links(doc)
+doc = format_internal_aliased_links(doc)
 
-2. The mount needs to be opened at startup. The command is:
-		
-		rclone --vfs-cache-mode writes mount "<name of config>":  ~/OneDrive
-		
-[[Some link|some alias]] and some more text, and whhoops, here is a [[some link#header link]], and another [[link|alias]]."""
-
-print(format_internal_links(doc))
+with open("/home/kmaasrud/Brain/Obsidian and GitHub Pages workflow 2.md", "w") as f:
+    f.write(doc)
