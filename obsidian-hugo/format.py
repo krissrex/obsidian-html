e181f3d22775ddabb2222aaba2cfdcd27fbdf1e0
kmaasrud <kmaasrud@outlook.com>
2020-08-05 18:51:47 +0200

    Header link formatting is up and working

--- obsidian-hugo/format.py
+++ obsidian-hugo/format.py
@@ -1,5 +1,9 @@
 import re
 
+def slug_case(text):
+    text = text.replace(".", "dot")
+    return re.sub(r'[\W_]+', '-', text)
+
 def format_internal_links(document):
     """Links that are neither aliased, nor link to headers"""
     return re.sub("\\[{2}([^|#]*?)\\]{2}", "[\\1]({{ ref \"\\1.md\" }})", document)
@@ -10,13 +14,21 @@ def format_internal_aliased_links(document):
 
 def format_internal_header_links(document):
     """Header links (TODO: Headers need to be slug case formatted, so this does not work right now)"""
-    return re.sub("\\[{2}([^|#\\]]*?)#(.*?)\\]{2}", "[\\2]({{ ref \"\\1.md#\\2\" }})", document)
+    matches = re.finditer("\\[{2}([^|#\\]]*?)#(.*?)\\]{2}", document)
 
-with open("/home/kmaasrud/Brain/Obsidian and GitHub Pages workflow.md") as f:
+    for match in matches:
+        link_text = match.group(2)
+        link = match.group(1) + ".md#" + slug_case(match.group(2))
+        full_link = "[" + link_text + "]({{< ref \"" + link + "\" >}})"
+        document = document.replace(match.group(), full_link)
+
+    return document
+
+with open("/home/kmaasrud/Brain/Just a test.md") as f:
     doc = f.read()
 
-doc = format_internal_links(doc)
-doc = format_internal_aliased_links(doc)
+doc = format_internal_links(
+    format_internal_aliased_links(
+        format_internal_header_links(doc)))
 
-with open("/home/kmaasrud/Brain/Obsidian and GitHub Pages workflow 2.md", "w") as f:
-    f.write(doc)
+print(doc)
