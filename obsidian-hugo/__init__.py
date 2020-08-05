886c2f7439547254dcd43e3e38af9bbe74e02478
kmaasrud <kmaasrud@outlook.com>
2020-08-05 20:23:49 +0200

    Func to find every file in vault. Main func to make the prog usable

--- obsidian-hugo/__init__.py
+++ obsidian-hugo/__init__.py
@@ -1,3 +1,5 @@
+import os
+import sys
 from format import *
 
 def format_file(file_name, vault_root):
@@ -13,4 +15,22 @@ def format_file(file_name, vault_root):
     with open(vault_root + "/hugo/content/" + file_name, "w") as f:
         f.write(new_doc)
 
-format_file("links.md", "/home/kmaasrud/Brain")
+def find_files(vault_root, extra_folders):
+    md_files = [f for f in os.listdir(vault_root) if f.endswith(".md")]
+    for folder in extra_folders:
+        md_files += [folder + "/" + f for f in os.listdir(vault_root + "/" + folder) if f.endswith(".md")]
+
+    return md_files
+
+def main(vault_root, extra_folders = []):
+    if not os.path.exists(vault_root + "/hugo/content"):
+        os.makedirs(vault_root + "/hugo/content")
+    for folder in extra_folders:
+        if not os.path.exists(vault_root + "/hugo/content/" + folder):
+            os.makedirs(vault_root + "/hugo/content/" + folder)
+
+    for md_file in find_files(vault_root, extra_folders):
+        format_file(md_file, vault_root)
+
+if __name__ == "__main__":
+    main(sys.argv[1], extra_folders = sys.argv[2:])
