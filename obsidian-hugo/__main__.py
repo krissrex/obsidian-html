a17721e71de480e6a291072bec8da06c271a1093
kmaasrud <kmaasrud@outlook.com>
2020-08-09 18:11:40 +0200

    Main file instead of init file

---
+++ obsidian-hugo/__main__.py
@@ -0,0 +1,38 @@
+import os
+import sys
+from .format import *
+
+def format_file(file_name, vault_root):
+    with open(vault_root + "/" + file_name) as f:
+        doc = f.read()
+
+    new_doc = format_internal_links(
+        format_internal_aliased_links(
+            format_internal_header_links(doc)))
+
+    new_doc = f"+++\ntitle = \"{file_name.replace('.md', '')}\"\n+++\n\n" + new_doc
+
+    with open(os.path.dirname(vault_root) + "/hugo/content/" + file_name, "w") as f:
+        f.write(new_doc)
+
+def find_files(vault_root, extra_folders):
+    md_files = [f for f in os.listdir(vault_root) if f.endswith(".md")]
+    for folder in extra_folders:
+        md_files += [folder + "/" + f for f in os.listdir(vault_root + "/" + folder) if f.endswith(".md")]
+
+    return md_files
+
+def main(vault_root, extra_folders = []):
+    parent_of_vault = os.path.dirname(vault_root)
+    if not os.path.exists(parent_of_vault + "/hugo"):
+        os.makedirs(parent_of_vault + "/hugo")
+        os.makedirs(parent_of_vault + "/hugo/content")
+    for folder in extra_folders:
+        if not os.path.exists(parent_of_vault + "/hugo/content/" + folder):
+            os.makedirs(parent_of_vault + "/hugo/content/" + folder)
+
+    for md_file in find_files(vault_root, extra_folders):
+        format_file(md_file, vault_root)
+
+if __name__ == "__main__":
+    main(sys.argv[1], extra_folders = sys.argv[2:])
