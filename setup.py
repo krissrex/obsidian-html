ce5efa799266dd9df4699fdd03a3af038a2aebc7
kmaasrud <kmaasrud@outlook.com>
2020-08-13 11:01:23 +0200

    Added dependencies to setup-file and renamed package

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup
 
-setup(name='obsidian-hugo',
-      version='0.1',
-      description='Makes Obsidian documents Hugo-friendly',
-      url='https://github.com/kmaasrud/obsidian-hugo',
-      author='kmaasrud',
-      author_email='kmaasrud@outlook.com',
-      license='MIT',
-      packages=['obsidian-hugo'],
-      zip_safe=False)
+setup(name='obsidian-html',
+    version='0.1',
+    description='Converts an Obsidian vault into HTML',
+    url='https://github.com/kmaasrud/obsidian-hugo',
+    author='kmaasrud',
+    author_email='kmaasrud@outlook.com',
+    license='MIT',
+    packages=['obsidian-html'],
+    install_requires=[
+        'markdown2'
+    ]
+    zip_safe=False)
