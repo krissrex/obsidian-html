5a61f36354a0d6c8b059f2c7e2b949afc721801f
kmaasrud <kmaasrud@outlook.com>
2020-08-13 11:26:01 +0200

    Dash character needed escaping in regex

--- obsidian-html/utils.py
+++ obsidian-html/utils.py
@@ -3,7 +3,7 @@ import re
 def slug_case(text):
     text = text.replace(".", "dot")
     text = text.replace("_", "-")
-    return re.sub(r'[^\w-/]+', '-', text)
+    return re.sub(r'[^\w\-/]+', '-', text)
 
 def md_link(text, link):
     return "[" + text + "](" + link + ")"
