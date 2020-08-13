a8cc0fa8ace601a37799ae03fb039363e0d07ecf
kmaasrud <kmaasrud@outlook.com>
2020-08-13 10:56:38 +0200

    File handler using markdown2 with extras

--- obsidian-hugo/file_handling.py
+++ obsidian-hugo/file_handling.py
@@ -1,17 +1,20 @@
 import os
+from format import format_internal_links, format_internal_aliased_links, format_internal_header_links, format_internal_header_links, format_tags
+from utils import slug_case
 
 def format_file(file_name, vault_root):
     with open(vault_root + "/" + file_name) as f:
         doc = f.read()
 
-    new_doc = format_internal_links(
+    doc = format_internal_links(
         format_internal_aliased_links(
-            format_internal_header_links(doc)))
+            format_internal_header_links(
+                format_tags(doc))))
 
-    new_doc = f"+++\ntitle = \"{file_name.replace('.md', '')}\"\n+++\n\n" + new_doc
+    html = markdown2.markdown(doc, extras=["break-on-newline, fenced-code-blocks, header-ids, strike, tables"]
 
-    with open(os.path.dirname(vault_root) + "/hugo/content/" + file_name, "w") as f:
-        f.write(new_doc)
+    with open(os.path.dirname(vault_root) + "/html/" + slug_case(file_name), "w") as f:
+        f.write(html)
 
 def find_files(vault_root, extra_folders):
     md_files = [f for f in os.listdir(vault_root) if f.endswith(".md")]
