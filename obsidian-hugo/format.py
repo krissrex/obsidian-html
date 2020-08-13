b36c7a2432a96fe50f551f6036eddf07346620c3
kmaasrud <kmaasrud@outlook.com>
2020-08-13 10:56:55 +0200

    Also formats tags

--- obsidian-hugo/format.py
+++ obsidian-hugo/format.py
@@ -1,25 +1,33 @@
-import re
+import regex as re
 from utils import slug_case, md_link
 
 def format_internal_links(document):
     """Links that are neither aliased, nor links to headers"""
     matches = re.finditer("\\[{2}([^|#]*?)\\]{2}", document)
 
-    return obsidian_to_commonmark_link(document, matches, no_groups = 1)
+    return obsidian_to_commonmark_links(document, matches, no_groups = 1)
 
 def format_internal_aliased_links(document):
     """Aliased links"""
     matches = re.finditer("\\[{2}([^|#\\]]*?)\\|(.*?)\\]{2}", document)
 
-    return obsidian_to_commonmark_link(document, matches)
+    return obsidian_to_commonmark_links(document, matches)
 
 def format_internal_header_links(document):
     """Header links (TODO: Headers need to be slug case formatted, so this does not work right now)"""
     matches = re.finditer("\\[{2}([^|#\\]]*?)#(.*?)\\]{2}", document)
 
-    return obsidian_to_commonmark_link(document, matches)
+    return obsidian_to_commonmark_links(document, matches)
 
-def obsidian_to_commonmark_link(document, matches, no_groups = 2):
+def format_tags(document):
+    matches = re.finditer(r"#([\p{L}_]+)", document)
+
+    for match in matches:
+        document = document.replace(match.group(), "<span class=\"tag\">" + match.group(1) + "</span>")
+
+    return document
+
+def obsidian_to_commonmark_links(document, matches, no_groups = 2):
     for match in matches:
         text = match.group(no_groups)
         link = slug_case(match.group(1))
