483b65c2059f3700fe4a255a5777c5b24d923f68
kmaasrud <kmaasrud@outlook.com>
2020-08-13 09:57:35 +0200

    Dropped Hugo formatting, now formats to CommonMark specs

--- obsidian-hugo/format.py
+++ obsidian-hugo/format.py
@@ -1,25 +1,28 @@
 import re
-
-def slug_case(text):
-    text = text.replace(".", "dot")
-    return re.sub(r'[\W_]+', '-', text)
+from utils import slug_case, md_link
 
 def format_internal_links(document):
-    """Links that are neither aliased, nor link to headers"""
-    return re.sub("\\[{2}([^|#]*?)\\]{2}", "[\\1]({{< ref \"\\1.md\" >}})", document)
+    """Links that are neither aliased, nor links to headers"""
+    matches = re.finditer("\\[{2}([^|#]*?)\\]{2}", document)
+
+    return obsidian_to_commonmark_link(document, matches)
 
 def format_internal_aliased_links(document):
     """Aliased links"""
-    return re.sub("\\[{2}([^|#\\]]*?)\\|(.*?)\\]{2}", "[\\2]({{< ref \"\\1.md\" >}})", document)
+    matches = re.finditer("\\[{2}([^|#\\]]*?)\\|(.*?)\\]{2}", document)
+
+    return obsidian_to_commonmark_link(document, matches)
 
 def format_internal_header_links(document):
     """Header links (TODO: Headers need to be slug case formatted, so this does not work right now)"""
     matches = re.finditer("\\[{2}([^|#\\]]*?)#(.*?)\\]{2}", document)
 
+    return obsidian_to_commonmark_link(document, matches)
+
+def obsidian_to_commonmark_link(document, matches):
     for match in matches:
-        link_text = match.group(2)
-        link = match.group(1) + ".md#" + slug_case(match.group(2))
-        full_link = "[" + link_text + "]({{< ref \"" + link + "\" >}})"
-        document = document.replace(match.group(), full_link)
+        text = match.group(2) if match.group(2) else match.group(1)
+        link = slug_case(match.group(1))
+        document = document.replace(match.group(), md_link(text, link))
 
     return document
