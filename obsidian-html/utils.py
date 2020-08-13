df2f5ffdb23b3cfcb0bfef306ad3140bccc89533
kmaasrud <kmaasrud@outlook.com>
2020-08-13 11:21:47 +0200

    Slug case func now supports forward slashes

--- obsidian-html/utils.py
+++ obsidian-html/utils.py
@@ -2,7 +2,8 @@ import re
 
 def slug_case(text):
     text = text.replace(".", "dot")
-    return re.sub(r'[\W_]+', '-', text)
+    text = text.replace("_", "-")
+    return re.sub(r'[^\w-/]+', '-', text)
 
 def md_link(text, link):
     return "[" + text + "](" + link + ")"
