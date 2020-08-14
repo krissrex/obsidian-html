4fb5815632f11a7dbc65acdf038adc65a6942ff9
kmaasrud <kmaasrud@outlook.com>
2020-08-14 09:44:48 +0200

    The tag formatter was a bit to greedy

--- obsidian_html/format.py
+++ obsidian_html/format.py
@@ -21,12 +21,17 @@ def format_internal_header_links(document):
     """Formats Obsidian style header links"""
     matches = re.finditer("\\[{2}([^|#\\]]*?)#(.*?)\\]{2}", document)
 
-    return obsidian_to_commonmark_links(document, matches)
+    for match in matches:
+        text = match.group(2)
+        link = slug_case(match.group(1)) + "#" + slug_case(match.group(2))
+        document = document.replace(match.group(), md_link(text, link))
+
+    return document
 
 
 def format_tags(document):
     """Obsidian style tags. Removes #-icon and adds a span tag."""
-    matches = re.finditer(r"#([\p{L}_]+)", document)
+    matches = re.finditer(r"\s#([\p{L}_]+)", document)
 
     for match in matches:
         document = document.replace(
@@ -46,10 +51,11 @@ def obsidian_to_commonmark_links(document, matches, no_groups=2):
 
 def htmlify(document):
     # Formatting of Obsidian tags and links.
-    document = format_internal_links(
-        format_internal_aliased_links(
-            format_internal_header_links(
-                format_tags(document))))
+    document = format_tags(
+        format_internal_links(
+            format_internal_aliased_links(
+                format_internal_header_links(
+                    document))))
 
     markdown2_extras = [
         # Parser should work withouth strict linebreaks.
--- obsidian_html/test.py
+++ obsidian_html/test.py
@@ -1,4 +1,6 @@
 from obsidian_html import Vault
 
-vault = Vault("/home/kmaasrud/Brain/vault")
+vault = Vault("/home/kmaasrud/Brain/vault",
+              extra_folders=["daily"], html_template="./template.html")
+
 vault.export_html("/home/kmaasrud/Brain/html")
