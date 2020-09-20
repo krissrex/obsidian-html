0d1cb9d54634b235957b7df194958f9116b664e3
kmaasrud <kmaasrud@outlook.com>
2020-09-20 22:25:58 +0200

    Entry point script for easier command line interaction

--- obsidian_html/__init__.py
+++ obsidian_html/__init__.py
@@ -1,2 +1,32 @@
-import __main__
+import sys
+import argparse
 from .Vault import Vault
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog="obsidian-html",
+        description="Converts an Obsidian vault into HTML")
+
+    parser.add_argument("Vault",
+                        metavar="vault",
+                        type=str,
+                        default=".",
+                        help="Path to the vault root")
+
+    parser.add_argument("-o", "--output_dir",
+                        default=sys.argv[1] + "/html",
+                        help="Path to place the generated HTML")
+
+    parser.add_argument("-t", "--template",
+                        default=None,
+                        help="Path to HTML template")
+
+    parser.add_argument("-d", "--dirs",
+                        nargs="+",
+                        default=[],
+                        help="Extra sub-directories in vault that you want included")
+
+    args = parser.parse_args()
+
+    vault = Vault(args.Vault, extra_folders=args.dirs, html_template=args.template)
+    vault.export_html(args.output_dir)
--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
 from setuptools import setup
 
-setup(name='obsidian-html',
-      version='0.1',
-      description='Converts an Obsidian vault into HTML',
-      url='https://github.com/kmaasrud/obsidian-hugo',
-      author='kmaasrud',
-      author_email='kmaasrud@outlook.com',
-      license='MIT',
-      packages=['obsidian_html'],
-      install_requires=[
-          'markdown2',
-          'regex'
-      ],
-      zip_safe=False)
+setup(
+    name='obsidian-html',
+    version='0.1',
+    description='Converts an Obsidian vault into HTML',
+    url='https://github.com/kmaasrud/obsidian-hugo',
+    author='kmaasrud',
+    author_email='kmaasrud@outlook.com',
+    license='MIT',
+    packages=['obsidian_html'],
+    install_requires=[
+      'markdown2',
+      'regex'
+    ],
+    zip_safe=False,
+    entry_points={
+        'console_scripts': [
+            obsidian-html=obsidian_html:main
+        ]
+    }
+)
